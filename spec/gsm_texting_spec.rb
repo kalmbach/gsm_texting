96706216fb1cdc8e8e7a7338ad7c69edd5287a5a
Jorge <kalmbach@gmail.com>
2020-10-17 12:27:51 -0300

    Transliterate some ISO 8859-1 characters

--- spec/gsm_texting_spec.rb
+++ spec/gsm_texting_spec.rb
@@ -12,27 +12,68 @@ RSpec.describe GSMTexting do
       expect(GSMTexting.can_encode?(extended_set)).to be true
     end
 
-    it "cannot encode incompatible unicode characters" do
-      unicode_characters = GSMTexting::COMPATIBILITY_TABLE.keys.pack("U*")
-      expect(GSMTexting.can_encode?(unicode_characters)).to be false
+    it "cannot encode compatible unicode characters" do
+      input_characters = GSMTexting::COMPATIBILITY_TABLE.keys.pack("U*")
+      expect(GSMTexting.can_encode?(input_characters)).to be false
+    end
+
+    it "cannot encode convertible characters" do
+      input_characters = GSMTexting::CONVERSION_TABLE.keys.pack("U*")
+      expect(GSMTexting.can_encode?(input_characters)).to be false
     end
   end
 
   describe "#encode" do
-    it "can convert unicode to gsm compatible chars" do
-      unicode_characters = GSMTexting::COMPATIBILITY_TABLE.keys.pack("U*")
-      expect(GSMTexting.can_encode?(unicode_characters)).to be false
+    it "can convert unicode characters to gsm compatible characters" do
+      input_characters = GSMTexting::COMPATIBILITY_TABLE.keys.pack("U*")
+      expect(GSMTexting.can_encode?(input_characters)).to be false
 
-      converted_characters = GSMTexting.encode(unicode_characters)
-      expect(GSMTexting.can_encode?(converted_characters)).to be true
+      encoded_characters = GSMTexting.encode(input_characters)
+      expect(GSMTexting.can_encode?(encoded_characters)).to be true
     end
 
-    it "can convert unicode chars to similar gsm chars" do
-      unicode_characters = "áéíóu"
-      expect(GSMTexting.can_encode?(unicode_characters)).to be false
-      converted_characters = GSMTexting.encode(unicode_characters)
-      puts "converted: " + converted_characters
-      expect(GSMTexting.can_encode?(converted_characters)).to be true
+    it "can transliterate unicode chars to compatible gsm characters" do
+      input_characters = GSMTexting::CONVERSION_TABLE.keys.pack("U*")
+      expect(GSMTexting.can_encode?(input_characters)).to be false
+
+      encoded_characters = GSMTexting.encode(input_characters, transliterate: true)
+      expect(GSMTexting.can_encode?(encoded_characters)).to be true
+    end
+  end
+
+  describe "replace_char argument" do
+    it "will use default replace character" do
+      input_characters = "ξ"
+      expect(GSMTexting.can_encode?(input_characters)).to be false
+
+      encoded_characters = GSMTexting.encode(input_characters)
+      expect(encoded_characters).to eq GSMTexting::DEFAULT_REPLACE_CHAR
+    end
+
+    it "will use provided char as replacement" do
+      input_characters = "ξ"
+      expect(GSMTexting.can_encode?(input_characters)).to be false
+
+      encoded_characters = GSMTexting.encode(input_characters, replace_char: "*")
+      expect(encoded_characters).to eq "*"
+    end
+  end
+
+  describe "transliterate argument" do
+    it "will not transliterate chars by default" do
+      input_characters = GSMTexting::CONVERSION_TABLE.keys.pack("U*")
+      expect(GSMTexting.can_encode?(input_characters)).to be false
+
+      encoded_characters = GSMTexting.encode(input_characters)
+      expect(encoded_characters).to eq "?" * input_characters.length
+    end
+
+    it "will transliterate chars if requested" do
+      input_characters = GSMTexting::CONVERSION_TABLE.keys.pack("U*")
+      expect(GSMTexting.can_encode?(input_characters)).to be false
+
+      encoded_characters = GSMTexting.encode(input_characters, transliterate: true)
+      expect(encoded_characters).to eq GSMTexting::CONVERSION_TABLE.values.join
     end
   end
 end
