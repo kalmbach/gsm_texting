29a28f615d6c45d0e3ca762483f1d66d7ba49c06
Jorge <kalmbach@gmail.com>
2020-11-11 13:16:00 -0300

    Add option to truncate string to fit into a single sms segment

--- spec/gsm_texting_spec.rb
+++ spec/gsm_texting_spec.rb
@@ -1,79 +1,64 @@
-require File.expand_path(File.dirname(__FILE__) + "/../lib/gsm_texting")
+# frozen_string_literal: true
+
+require File.expand_path([File.dirname(__FILE__), '/../lib/gsm_texting'].join)
 
 RSpec.describe GSMTexting do
-  describe "#can_encode?" do
-    it "can encode basic set of characters" do
-      basic_set = GSMTexting::BASIC_CHARACTER_SET.join
-      expect(GSMTexting.can_encode?(basic_set)).to be true
+  let(:long_message) do
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'\
+    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"\
+    'when an unknown printer took a galley of type and scrambled it to make a type'\
+    'specimen book'
+  end
+
+  let(:truncated_message) do
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'\
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"\
+      'when an...'
+  end
+
+  describe 'can_encode?' do
+    it 'returns true if text can be encoded' do
+      expect(GSMTexting.can_encode?('hola')).to be true
     end
 
-    it "can encode extended set of characters" do
-      extended_set = GSMTexting::BASIC_CHARACTER_SET_EXTENSION.join
-      expect(GSMTexting.can_encode?(extended_set)).to be true
-    end
-
-    it "cannot encode compatible unicode characters" do
-      input_characters = GSMTexting::COMPATIBILITY_TABLE.keys.pack("U*")
-      expect(GSMTexting.can_encode?(input_characters)).to be false
-    end
-
-    it "cannot encode convertible characters" do
-      input_characters = GSMTexting::CONVERSION_TABLE.keys.pack("U*")
-      expect(GSMTexting.can_encode?(input_characters)).to be false
+    it 'returns false if text cannot be encoded' do
+      expect(GSMTexting.can_encode?('ɴ⇼')).to be false
     end
   end
 
-  describe "#encode" do
-    it "can convert unicode characters to gsm compatible characters" do
-      input_characters = GSMTexting::COMPATIBILITY_TABLE.keys.pack("U*")
-      expect(GSMTexting.can_encode?(input_characters)).to be false
-
-      encoded_characters = GSMTexting.encode(input_characters)
-      expect(GSMTexting.can_encode?(encoded_characters)).to be true
+  describe 'encode' do
+    it 'can perform smart substitutions' do
+      expect(GSMTexting.encode('ɴo')).to eq 'No'
     end
 
-    it "can transliterate unicode chars to compatible gsm characters" do
-      input_characters = GSMTexting::CONVERSION_TABLE.keys.pack("U*")
-      expect(GSMTexting.can_encode?(input_characters)).to be false
-
-      encoded_characters = GSMTexting.encode(input_characters, transliterate: true)
-      expect(GSMTexting.can_encode?(encoded_characters)).to be true
-    end
-  end
-
-  describe "replace_char argument" do
-    it "will use default replace character" do
-      input_characters = "ξ"
-      expect(GSMTexting.can_encode?(input_characters)).to be false
-
-      encoded_characters = GSMTexting.encode(input_characters)
-      expect(encoded_characters).to eq GSMTexting::DEFAULT_REPLACE_CHAR
+    it 'will replace not compatible chars with default char' do
+      expect(GSMTexting.encode('Sí')).to eq 'S?'
     end
 
-    it "will use provided char as replacement" do
-      input_characters = "ξ"
-      expect(GSMTexting.can_encode?(input_characters)).to be false
+    it 'will replace not compatible chars with provided char' do
+      expect(GSMTexting.encode('Sí', replace_char: '*')).to eq 'S*'
+    end
 
-      encoded_characters = GSMTexting.encode(input_characters, replace_char: "*")
-      expect(encoded_characters).to eq "*"
+    it 'can perform transliterations' do
+      expect(GSMTexting.encode('Sí', transliterate: true)).to eq 'Si'
+    end
+
+    it 'can truncate large messages into a single segment' do
+      expect(GSMTexting.encode(long_message, truncate: true)).to eq truncated_message
     end
   end
 
-  describe "transliterate argument" do
-    it "will not transliterate chars by default" do
-      input_characters = GSMTexting::CONVERSION_TABLE.keys.pack("U*")
-      expect(GSMTexting.can_encode?(input_characters)).to be false
-
-      encoded_characters = GSMTexting.encode(input_characters)
-      expect(encoded_characters).to eq "?" * input_characters.length
+  describe 'single_segment?' do
+    it 'returns true if message fits into a single sms segment' do
+      expect(GSMTexting.single_segment?('Hola')).to be true
     end
 
-    it "will transliterate chars if requested" do
-      input_characters = GSMTexting::CONVERSION_TABLE.keys.pack("U*")
-      expect(GSMTexting.can_encode?(input_characters)).to be false
+    it 'returns false if message doesnt fit into a single sms segment' do
+      expect(GSMTexting.single_segment?(long_message)).to eq false
+    end
 
-      encoded_characters = GSMTexting.encode(input_characters, transliterate: true)
-      expect(encoded_characters).to eq GSMTexting::CONVERSION_TABLE.values.join
+    it 'raises an error if  message cannot be encoded' do
+      expect { GSMTexting.single_segment?('ɴ⇼') }.to raise_error GSMTexting::InvalidGSMString
     end
   end
 end
